18043009785833560b1cfdf051db68c997845d8a
nesteg <evgnest5@gmail.com>
2020-07-04 09:57:35 +0300

    Create Hibernate application with Cache

---
+++ HW11-Hibernate/src/main/java/ru/otus/hw11/core/model/PhoneDataSet.java
@@ -0,0 +1,49 @@
+package ru.otus.hw11.core.model;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "phones")
+public class PhoneDataSet {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "phone_id")
+    private long id;
+
+    @Column(name = "number")
+    private String number;
+
+
+    @ManyToOne(cascade = CascadeType.ALL,fetch =FetchType.LAZY)
+    @JoinColumn(name = "user_id",referencedColumnName = "user_id")
+    private User user;
+
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+
+
+
+    public PhoneDataSet() {
+    }
+
+    public PhoneDataSet(long id, String number) {
+        this.id = id;
+        this.number = number;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+}
